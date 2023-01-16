fa5f34e53ebe4e5f4a8f7856927adea7064dcdce
kaizerenrique <kayserenrique@gmail.com>
2023-01-16 14:00:59 -0400

    habitad menu lateral

--- resources/views/livewire/menu/menulateral.blade.php
+++ resources/views/livewire/menu/menulateral.blade.php
@@ -72,7 +72,7 @@
             </li> 
 
             <li>
-                <a href="#"
+                <a href="{{ route('habitad') }}"
                     class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-100">                    
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-gray-600 transition duration-75 dark:text-gray-500 group-hover:text-gray-900 dark:group-hover:text-white">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
