ab2008de3378af0d6b08bc000811dd2341b9ca68
kaizerenrique <kayserenrique@gmail.com>
2023-01-22 19:55:32 -0400

    nueva familia

--- app/Http/Livewire/Admin/Nuevafamiliacomponente.php
+++ app/Http/Livewire/Admin/Nuevafamiliacomponente.php
@@ -4,14 +4,171 @@ namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
 use Livewire\WithPagination;
+use App\Models\ConsejoComunal;
+use App\Models\Clap;
+use App\Models\Direccion;
+use App\Models\Habitad;
 use App\Models\Familia;
+use App\Models\Persona;
 
 class Nuevafamiliacomponente extends Component
 {
     use WithPagination;
     
+    //variables
+    public $consejocomunal , $clap , $direccion , $habitad, $codigo, $idfamilia;
+    public $consejocomunales = [] , $claps = [] , $direcciones = [] , $habitads = [];
+
+    public $modalPersona = false;
+    public $estado = false;
+
+    public $familialistado = [];
+    public $integrante;
+
+    //barra de busqueda
+    public $buscar;
+
+    //componentes para la tabla de personas
+    protected $queryString = [
+        'buscar' => ['except' => '']
+    ];
+
+    public function mount()
+    {
+        $this->consejocomunales = ConsejoComunal::all();
+        $this->claps = collect();
+        $this->direcciones = collect();
+        $this->habitads = collect();
+    }
+
+    public function updatedConsejocomunal($value)
+    {
+        $this->claps = Clap::where('consejo_comunal_id', $value )->get();
+        $this->clap = $this->claps->first()->id ?? null;
+
+        $this->direcciones = Direccion::where('consejo_comunal_id', $value )->get();
+        $this->direccion = $this->direcciones->first()->id ?? null;
+    }
+
+    public function updatedDireccion($value)
+    {
+        $this->habitads = Habitad::where('direccion_id', $value )->get();
+        $this->habitad = $this->habitads->first()->id ?? null;
+    }
+
+    public function registrar()
+    {
+        $this->validate([
+            'consejocomunal' => 'required', 
+            'clap' => 'required', 
+            'direccion' => 'required', 
+            'habitad' => 'required'
+        ]);
+
+        $this->codigo = uniqid('Familia-');
+
+        $familia = new Familia();
+        $familia->codigo = $this->codigo; 
+        $familia->status = 'Procesando';
+        $familia->consejo_comunal_id = $this->consejocomunal;
+        $familia->clap_id = $this->clap;
+        $familia->habitad_id = $this->habitad;
+        $familia->save();
+
+    }
+
+    public function listadepersonas()
+    {
+        $this->modalPersona = true;
+    }
+
+    public function agregarpersona($id)
+    {
+        $familia = Familia::where('codigo', $this->codigo)->get();
+
+        foreach ($familia as $famil) {
+            $resultado = $famil->id;
+        }
+        
+        $integrantes = Familia::find($resultado);
+        $integrantes->personas;
+
+        //dd($integrantes->personas);
+
+        if (!empty($integrantes->personas)) {
+            $persona = Persona::find($id);        
+            $persona->familia_id = $resultado;
+            $persona->save();
+        } else {
+            dd($integrantes->personas);
+        }
+    }
+
+    public function statusfamilia()
+    {
+        $familia = Familia::where('codigo', $this->codigo)->get();   
+        foreach ($familia as $famil) {
+            $idfamilia = $famil->id;
+        } 
+
+        $integran = Persona::where('familia_id', $idfamilia)->count();        
+
+        if ($integran == 0) {
+            $famil = Familia::find($idfamilia);
+            $famil->status = 'Pendiente';
+            $famil->save();
+        } else {
+            $famil = Familia::find($idfamilia);
+            $famil->status = 'Completado';            
+            $famil->save();
+        }
+
+        
+
+        return redirect()->route('familias');
+    }
+    
+
     public function render()
     {
-        return view('livewire.admin.nuevafamiliacomponente');
+        $personas = Persona::where('familia_id', null)
+                    ->when($this->buscar, function($query){
+                        return $query->where(function ($query)
+                        {
+                            $query->where('cedula', 'like', '%'.$this->buscar . '%')
+                            ->orWhere('nombres', 'like', '%'.$this->buscar . '%') //buscar por nombres
+                            ->orWhere('apellidos', 'like', '%'.$this->buscar . '%') //buscar por apellidos
+                            ->orderBy('id','desc'); //ordenar de forma decendente
+                        });
+                    });
+        $personas = $personas->paginate(5);
+
+        if (!empty($this->codigo)) {
+            $familia = Familia::where('codigo', $this->codigo)->get();   
+            foreach ($familia as $famil) {
+                $idfamilia = $famil->id;
+            } 
+
+            
+            $integran = Persona::where('familia_id', $idfamilia)->get();
+
+            if (!empty($integran)) {
+                $integrantes = $integran;
+            } else {
+                $integrantes = null;
+            }
+            
+              
+        } else {
+            $integrantes = null;
+        }
+        
+
+        
+
+        return view('livewire.admin.nuevafamiliacomponente',[
+            'personas' => $personas,
+            'integrantes' => $integrantes,
+        ]);
     }
 }
--- resources/views/livewire/admin/nuevafamiliacomponente.blade.php
+++ resources/views/livewire/admin/nuevafamiliacomponente.blade.php
@@ -1,3 +1,225 @@
 <div class="p-2">
-    {{-- Care about people's approval and you will be their prisoner. --}}
+    <div class="mt-4">
+        <div class="mt-4 text-2xl flex justify-between">
+            <div class="mt-4 mb-4 text-2xl flex justify-between leading-normal">
+                <span>Registrar Familia</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="px-4">
+        <!-- seccion -->
+        <div class="text-2xl flex justify-between">
+            <div class="mb-4 text-xl text-gray-600 flex justify-between leading-normal">
+                <span>Datos de Dirección</span>
+            </div>
+        </div>
+        <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-4">
+            <div class="col-span-2 sm:col-span-2 md:col-span-4 lg:col-span-6 xl:col-span-4">
+                <x-jet-label for="consejocomunal" value="{{ __('Consejo consejocomunal') }}" />
+                <select wire:model="consejocomunal" id="consejocomunal" name="consejocomunal" class="mt-1 block w-full border border-solid border-gray-300 rounded-lg text-gray-600 text-sm font-light">
+                    <option value="" selected>Seleccione el Consejo consejocomunal</option>
+                    @foreach ($consejocomunales as $consejocomunal)                            
+                        <option value="{{ $consejocomunal->id }}">{{ $consejocomunal->name }}</option>                            
+                    @endforeach                        
+                </select>
+            </div>
+        
+            <div class="col-span-2 sm:col-span-2 md:col-span-4 lg:col-span-6 xl:col-span-4">
+                <x-jet-label for="clap" value="{{ __('Clap') }}" />
+                <select wire:model="clap" class="mt-1 block w-full border border-solid border-gray-300 rounded-lg text-gray-600 text-sm font-light">
+                    @if ($claps->count() == 0)
+                        <option value="" selected>Seleccione el CLAP</option>
+                    @endif                        
+                    @foreach ($claps as $clap)                            
+                        <option value="{{ $clap->id }}">{{ $clap->name }}</option>                            
+                    @endforeach                        
+                </select>
+            </div>
+        
+            <div class="col-span-2 sm:col-span-2 md:col-span-2 lg:col-span-3 xl:col-span-4">
+                <x-jet-label for="direccion" value="{{ __('Dirección') }}" />
+                <select wire:model="direccion" class="mt-1 block w-full border border-solid border-gray-300 rounded-lg text-gray-600 text-sm font-light">
+                    @if ($direcciones->count() == 0)
+                        <option value="" selected>Seleccione la Direcciónado</option>
+                    @endif                        
+                    @foreach ($direcciones as $direccion)                            
+                        <option value="{{ $direccion->id }}">{{ $direccion->direccion }}</option>                            
+                    @endforeach                        
+                </select>
+            </div>
+        
+            <div class="col-span-2 sm:col-span-2 md:col-span-2 lg:col-span-3 xl:col-span-4">
+                <x-jet-label for="habitad" value="{{ __('Casa / Apartamento / Habitación ') }}" />
+                <select wire:model="habitad" class="mt-1 block w-full border border-solid border-gray-300 rounded-lg text-gray-600 text-sm font-light">
+                    @if ($habitads->count() == 0)
+                        <option value="" selected>Seleccione la Casa / Apartamento / Habitación </option>
+                    @endif                        
+                    @foreach ($habitads as $habitad)                            
+                        <option value="{{ $habitad->id }}">{{ $habitad->habitad}} -{{ $habitad->literal}}</option>                            
+                    @endforeach                        
+                </select>
+            </div>
+
+            <div class="col-span-2 sm:col-span-2">
+                <x-jet-label for="name" value="{{ __('Código') }}" />
+                <x-jet-input id="codigo" type="text" class="mt-1 block w-full border border-solid border-gray-300 rounded-lg text-gray-600 text-sm font-light" wire:model.defer="codigo" disabled/>
+                <x-jet-input-error for="codigo" class="mt-2" />
+            </div>
+
+            <div class="col-span-2 sm:col-span-2 pt-4">
+                <button type="button" class="focus:outline-none text-white text-base py-2.5 px-5 rounded-md bg-blue-400 hover:bg-blue-600 hover:shadow-lg flex items-center"
+                    wire:click="registrar" wire:loading.attr="disabled">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6 mr-2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
+                    </svg>
+                    {{ __('Generar Código') }}
+                </button>
+            </div>
+        </div>        
+    </div>
+
+    <div class="px-4">
+        @if (!empty($integrantes))
+            <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">            
+                <!-- Tabla -->
+                <table class="min-w-max w-full table-auto mt-6">
+                    <thead>
+                        <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                            <th class="px-4 py-2">
+                                <div class="flex items-center">Cedula</div>
+                            </th>
+                            <th class="px-4 py-2">
+                                <div class="flex items-center">Nombre y Apellido</div>
+                            </th>
+                            <th class="px-4 py-2">
+                                <div class="flex items-center">Edad</div>
+                            </th>
+                            <th class="px-4 py-2">
+                                <div class="flex items-center">J. Familia</div>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="text-gray-600 text-sm font-light">
+                        @foreach ($integrantes as $integrante)
+                            <tr class="border-b border-gray-300 hover:bg-gray-200">
+                                <td class="px-4 py-2">
+                                    <span>{{ $integrante->nacionalidad }}-{{ $integrante->cedula }}</span>
+                                </td>
+                                <td class="px-4 py-2">                                    
+                                    {{ $integrante->nombres }} {{ $integrante->apellidos }}                                    
+                                </td>
+                                <td class="px-4 py-2">
+                                    @if ($integrante->fnacimiento)
+                                        {{ \Carbon\Carbon::parse($integrante->fnacimiento)->age }}
+                                    @else
+                                        <p>No registrado</p>
+                                    @endif
+                                </td>
+                                <td class="px-4 py-2 text-center">
+                                    <div class="flex item-center">
+                                        @if ($integrante->jefedefamilia == 1)
+                                            <span class="bg-green-200 text-green-700 py-1 px-3 rounded-full text-xs">Si</span>
+                                        @else
+                                            <span class="bg-red-200 text-red-700 py-1 px-3 rounded-full text-xs">No</span>
+                                        @endif
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            
+        @endif 
+    </div>
+    
+    <!-- Inicio del Modal para mensajes y alertas  -->
+    <x-jet-dialog-modal wire:model="modalPersona">
+        <x-slot name="title">
+            {{ 'titulo' }}
+        </x-slot>
+
+        <x-slot name="content">
+            <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+                <!-- Busqueda -->
+                <div class="flex justify-between">
+                    <div>
+                        <input wire:model="buscar" type="search" placeholder="Buscar"
+                            class="shadow appearence-none border border-solid border-gray-300 rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline placeholder-indigo-500"
+                            name="">
+                    </div>
+                </div>
+                <!-- Tabla -->
+                <table class="min-w-max w-full table-auto mt-6">
+                    <thead>
+                        <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                            <th class="px-4 py-2">
+                                <div class="flex items-center">Cedula</div>
+                            </th>
+                            <th class="px-4 py-2">
+                                <div class="flex items-center">Nombre y Apellido</div>
+                            </th>
+                            <th class="px-4 py-2">
+                                <div class="flex items-center">Edad</div>
+                            </th>
+                            <th class="px-4 py-2">
+                                <div class="flex items-center">J. Familia</div>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="text-gray-600 text-sm font-light">
+                        @foreach ($personas as $persona)
+                            <tr class="border-b border-gray-300 hover:bg-gray-200" type="button" wire:click="agregarpersona({{$persona->id}})">
+                                <td class="px-4 py-2">
+                                    <span>{{ $persona->nacionalidad }}-{{ $persona->cedula }}</span>
+                                </td>
+                                <td class="px-4 py-2">                                    
+                                    {{ $persona->nombres }} {{ $persona->apellidos }}                                    
+                                </td>
+                                <td class="px-4 py-2">
+                                    @if ($persona->fnacimiento)
+                                        {{ \Carbon\Carbon::parse($persona->fnacimiento)->age }}
+                                    @else
+                                        <p>No registrado</p>
+                                    @endif
+                                </td>
+                                <td class="px-4 py-2 text-center">
+                                    <div class="flex item-center">
+                                        @if ($persona->jefedefamilia == 1)
+                                            <span class="bg-green-200 text-green-700 py-1 px-3 rounded-full text-xs">Si</span>
+                                        @else
+                                            <span class="bg-red-200 text-red-700 py-1 px-3 rounded-full text-xs">No</span>
+                                        @endif
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <div class="mt-4">
+                {{ $personas->links() }}
+            </div>
+        </x-slot>
+
+        <x-slot name="footer">
+            <x-jet-secondary-button wire:click="$toggle('modalPersona', false)" wire:loading.attr="disabled">
+                {{ __('Aceptar') }}
+            </x-jet-secondary-button>
+        </x-slot>
+    </x-jet-dialog-modal>
+
+    <div class="mt-4 px-4"> 
+        <x-jet-secondary-button wire:click="listadepersonas" wire:loading.attr="disabled">
+            {{ __('Agregar Personas') }}
+        </x-jet-secondary-button>
+        <x-jet-secondary-button wire:click="statusfamilia" wire:loading.attr="disabled">
+            {{ __('Aceptar') }}
+        </x-jet-secondary-button>
+        
+    </div>
+
+    
 </div>
