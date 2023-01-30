cf0c6ae3d404894aff6cc9c8ea3451b62c59af4f
kaizerenrique <kayserenrique@gmail.com>
2023-01-30 02:09:56 -0400

    Mensaje de Email para enviar el link de ingreso

--- resources/views/emails/registrousuario.blade.php
+++ resources/views/emails/registrousuario.blade.php
@@ -8,6 +8,9 @@
 
 Es importante que inicie sesión y confirme su correo electrónico, una vez confirmado se recomienda ir al perfil y cambiar la contraseña.
 
+@component('mail::button', ['url' => 'https://siscamve.xyz/login'])
+Ingresar.
+@endcomponent
 
 Gracias,<br>
 © {{ date('Y') }} {{ config('app.name') }}
